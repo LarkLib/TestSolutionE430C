731472dc15fa24c7f8c79c9646656771983ef31a
dev-alekliu <alek.liu@xero.com>
2018-04-13 18:05:40 +0800

    commint source code

--- TestjsonConsoleApplication/Program.cs
+++ TestjsonConsoleApplication/Program.cs
@@ -3,8 +3,11 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,21 +53,80 @@ namespace TestjsonConsoleApplication
                 // {"ExpiryDate":new Date(1230375600000),"Price":0}
             }
             //////////////////////////////////////////////////////////////////////////////////////////////////////////
-            var jsonString = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "houfuquan.js"));
+            //var jsonString = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "houfuquan.js"));
+            var jsonString = new WebClient().DownloadString("http://finance.sina.com.cn/realstock/company/sh600020/houfuquan.js");
             dynamic jArray = (JArray)JsonConvert.DeserializeObject(jsonString);
             dynamic jArray2 = JArray.Parse(jsonString);
             Console.WriteLine(jArray[0].total);
             foreach (var item in jArray[0].data)
             {
-                Console.WriteLine($"{item.Name}={item.Value}");
+                Console.WriteLine($"{DateTimeOffset.ParseExact(item.Name, "_yyyy_MM_dd", null)}={item.Value}");
+                break;
             }
             var houfaquanObj = JsonConvert.DeserializeObject<IList<Houfuquan>>(jsonString);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            var client = new WebClient();
+            var stockContent = client.DownloadString("http://api.finance.ifeng.com/akdaily/?code=sh600020&type=last");
+            dynamic jArrayAkdaily = (JObject)JsonConvert.DeserializeObject(stockContent);
+            JObject jArrayAkdaily2 = JObject.Parse(stockContent);
+            DataTable dt = new DataTable("akdaily");
+            var code = "sh600020";
+            dt.Columns.AddRange(
+                new[]
+                {
+                    new DataColumn("DateTime", typeof(DateTimeOffset)),
+                    new DataColumn("Code", typeof(string)),
+                    new DataColumn("OpenPrice", typeof(decimal)),
+                    new DataColumn("HighestPrice", typeof(decimal)),
+                    new DataColumn("ClosePrice", typeof(decimal)),
+                    new DataColumn("LowestPrice", typeof(decimal)),
+                    new DataColumn("Volume", typeof(long)),
+                    new DataColumn("PriceChange", typeof(decimal)),
+                    new DataColumn("PercentChange", typeof(decimal)),
+                    new DataColumn("MA5", typeof(decimal)),
+                    new DataColumn("MA10", typeof(decimal)),
+                    new DataColumn("MA20", typeof(decimal)),
+                    new DataColumn("VolumeMA5", typeof(decimal)),
+                    new DataColumn("VolumeMA10", typeof(decimal)),
+                    new DataColumn("VolumeMA20", typeof(decimal)),
+                    new DataColumn("Turnover", typeof(decimal)),
+                });
+            foreach (JArray item in jArrayAkdaily2["record"])
+            {
+                var dataRow = dt.NewRow();
+                dt.Rows.Add(new object[] { (DateTimeOffset)item[0], code, (decimal)item[1], (decimal)item[2], (decimal)item[3], (decimal)item[4], (long)((decimal)item[5]) * 100, (decimal)item[6], (decimal)item[7], (decimal)item[8], (decimal)item[9], (decimal)item[10], (decimal)item[11], (decimal)item[12], (decimal)item[13], (decimal)item[14] });
+            }
+            var bulkCopy = new SqlBulkCopy("");
+            foreach (DataColumn item in dt.Columns)
+            {
+                bulkCopy.ColumnMappings.Add(item.ColumnName, item.ColumnName);
+            }
+            //bulkCopy.ColumnMappings.Add("DateTime", "DateTime");
+            //bulkCopy.ColumnMappings.Add("Code", "Code");
+            //bulkCopy.ColumnMappings.Add("OpenPrice", "OpenPrice");
+            //bulkCopy.ColumnMappings.Add("HighestPrice", "HighestPrice");
+            //bulkCopy.ColumnMappings.Add("LowestPrice", "LowestPrice");
+            //bulkCopy.ColumnMappings.Add("ClosePrice", "ClosePrice");
+            //bulkCopy.ColumnMappings.Add("PriceChange", "PriceChange");
+            //bulkCopy.ColumnMappings.Add("PercentChange", "PercentChange");
+            //bulkCopy.ColumnMappings.Add("TurnoverRate", "TurnoverRate");
+            //bulkCopy.ColumnMappings.Add("Volume", "Volume");
+            //bulkCopy.ColumnMappings.Add("MA5", "MA5");
+            //bulkCopy.ColumnMappings.Add("MA10", "MA10");
+            //bulkCopy.ColumnMappings.Add("MA20", "MA20");
+            //bulkCopy.ColumnMappings.Add("VolumeMA5", "VolumeMA5");
+            //bulkCopy.ColumnMappings.Add("VolumeMA10", "VolumeMA10");
+            //bulkCopy.ColumnMappings.Add("VolumeMA20", "VolumeMA20");
+
         }
 
         public class Houfuquan
         {
-            public int total { get; set; }
-            public Dictionary<string, decimal> data { get; set; }
+            //https://www.newtonsoft.com/json/help/html/T_Newtonsoft_Json_JsonPropertyAttribute.htm
+            [JsonProperty("total", Order = 1)]
+            public int Total { get; set; }
+            [JsonProperty("data", Order = 2)]
+            public Dictionary<string, decimal> Data { get; set; }
         }
         public class Product
         {
