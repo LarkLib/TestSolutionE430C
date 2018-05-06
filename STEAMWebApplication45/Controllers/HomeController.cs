fb4a9f02ec350f1f7b9147dd350ea63deff3877a
dev-alekliu <alek.liu@xero.com>
2018-05-06 14:41:29 +0800

    update

---
+++ STEAMWebApplication45/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ClassicAP()
+        {
+            return View();
+        }
+        public ActionResult ClassicAPP()
+        {
+            return View();
+        }
+        public ActionResult ClassicPython()
+        {
+            return View();
+        }
+        public ActionResult CourseCatalogue()
+        {
+            return View();
+        }
+        public ActionResult CourseSchedule()
+        {
+            return View();
+        }
+        public ActionResult Teachers()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
